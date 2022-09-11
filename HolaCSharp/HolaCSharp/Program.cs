74ba3a0cc8e8918648ef4c1c667e7bfbcc1639b8
Byron Quezada <byque2013@gmail.com>
2022-09-11 12:29:58 -0500

    Añadir hola mundo en C#

---
+++ HolaCSharp/HolaCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿/* 
+ * A partir de .NET 6 se ha simplificado la plantilla de aplicación de Consola.
+ * Ver https://docs.microsoft.com/es-mx/dotnet/core/tutorials/top-level-templates
+ * para más información.
+ */
+
+Console.WriteLine("Hola mundo C#");
