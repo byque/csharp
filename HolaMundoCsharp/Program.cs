09000c4328e48c79f005b55161ac3abe2a569c37
Byron Quezada <byque2013@gmail.com>
2022-10-31 00:02:19 -0500

    Agregar Hola Mundo C#
    Sin instrucciones de nivel superior

---
+++ HolaMundoCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HolaMundoCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo C#!");
+        }
+    }
+}
